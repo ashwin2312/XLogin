4c3aa836613fbe3a590e9e0e82346e22a60cf98d
Ashwin <ashvinkumar8891@gmail.com>
2025-01-31 22:45:31 +0530

    welcome message1

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
       {submitFail && <p>Invalid username or password</p>}
 
       {submitPass ? (
-        <p>Welcome user!</p>
+        <p>Welcome user</p>
       ) : (
         <form action="" onSubmit={handleSubmit}>
           <label htmlFor="username">Username:</label>
